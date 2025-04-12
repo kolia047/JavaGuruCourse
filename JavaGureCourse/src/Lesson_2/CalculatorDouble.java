e43c8588a9dfe683a8e600e4c64fd6e3e68a4c02
Nikolay Pervushin <kolia047@mail.ru>
2025-04-12 14:22:32 +0500

    LESSON2 calculator double

---
+++ JavaGureCourse/src/Lesson_2/CalculatorDouble.java
@@ -0,0 +1,14 @@
+package Lesson_2;
+
+public class CalculatorDouble {
+    public static void main(String[] args) {
+        double num1 = 165.25;
+        double num2 = 3269.00;
+
+        double multiply = num1 * num2;
+        double divide = num2/num1;
+
+        System.out.println(multiply);
+        System.out.println(divide);
+    }
+}
