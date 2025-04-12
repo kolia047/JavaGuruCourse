253de9906a17f31ee1d49261841822e9bd09cb75
Nikolay Pervushin <kolia047@mail.ru>
2025-04-12 14:11:59 +0500

    LESSON2 summa of numbers

---
+++ JavaGureCourse/src/Lesson_2/SumExample.java
@@ -0,0 +1,10 @@
+package Lesson_2;
+
+public class SumExample {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 200;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
