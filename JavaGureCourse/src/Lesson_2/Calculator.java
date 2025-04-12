78caf013bc735254e781932c4a6bfc10de2e71c5
Nikolay Pervushin <kolia047@mail.ru>
2025-04-12 14:12:46 +0500

    LESSON2 calculator of numbers

---
+++ JavaGureCourse/src/Lesson_2/Calculator.java
@@ -0,0 +1,14 @@
+package Lesson_2;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int num1 = 165;
+        int num2 = 3269;
+
+        int multiply = num1 * num2;
+        int divide = num2/num1;
+
+        System.out.println(multiply);
+        System.out.println(divide);
+    }
+}
