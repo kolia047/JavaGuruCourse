59b2738336202c26aaae5ea671ae1b5fb957313b
Nikolay Pervushin <kolia047@mail.ru>
2025-04-17 20:39:55 +0500

    LESSON_7 Array

---
+++ JavaGureCourse/src/Lesson_7/IntArrayExample.java
@@ -0,0 +1,24 @@
+package Lesson_7;
+
+import java.util.Arrays;
+
+public class IntArrayExample {
+    public static void main(String[] args) {
+        int[] marks = new int[5];
+        marks[0] = 25;
+        marks[1] = 225;
+        marks[2] = 251;
+        marks[3] = 125;
+        marks[4] = 2525;
+        System.out.println(Arrays.toString(marks));
+        System.out.println(marks.length);
+
+        int[] arr = {25,36,98,74,15,12,11,10};
+
+
+
+
+
+
+    }
+}
