0282782cc57c7e1e2fc81ed083812c046249e4c2
Nikolay Pervushin <kolia047@mail.ru>
2025-04-22 22:03:44 +0500

    Lesson_9 add class mark

---
+++ JavaGureCourse/src/Lesson_9/Mark.java
@@ -0,0 +1,19 @@
+package Lesson_9;
+
+ class Mark {
+    private String subject;
+    private int mark;
+
+    public Mark(String subject, int mark) {
+        this.subject = subject;
+        this.mark = mark;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+}
