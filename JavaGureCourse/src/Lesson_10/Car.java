96cf1ad1f6d9758e33c5d227517db80681402c6c
Nikolay Pervushin <kolia047@mail.ru>
2025-04-24 00:11:52 +0500

    Lesson_10 add class Car and class Ferrary

---
+++ JavaGureCourse/src/Lesson_10/Car.java
@@ -0,0 +1,12 @@
+package Lesson_10;
+
+public class Car {
+    private String color;
+
+    public Car(String color) {
+        this.color = color;
+    }
+    public void changeColor(String newColor){
+        this.color = newColor;
+    }
+}
---
+++ JavaGureCourse/src/Lesson_10/Ferrary.java
@@ -0,0 +1,9 @@
+package Lesson_10;
+
+public class Ferrary extends Car{
+
+    public Ferrary(String color) {
+        super(color);
+    }
+
+}
